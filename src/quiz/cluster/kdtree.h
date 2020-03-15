9d9a3f38e5da4257c241264f75fc7f5a0f5271f3
dongkesi <skd2278@gmail.com>
2020-03-16 00:30:26 +0800

    quiz: insert point; searching points in a KD-Tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -25,17 +25,84 @@ struct KdTree
 	: root(NULL)
 	{}
 
+	void helperInsert(Node *&node, std::vector<float> point, int id, int depth) {
+		if (node == NULL) {
+			node = new Node(point, id);
+		} else {
+			int dim =  depth % 2; // 0:x; 1:y
+			if (point[dim] < node->point[dim]) {
+				helperInsert(node->left, point, id, depth+1);
+			} else {
+				helperInsert(node->right, point, id, depth+1);
+			}
+		}
+	}
+
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
-
+		helperInsert(root, point, id, 0);
 	}
 
+	bool isInBox(std::vector<float> &point, std::vector<float> &target, float distanceTol) {
+
+		if (abs(point[0]-target[0]) <= distanceTol && abs(point[1] - target[1]) <= distanceTol) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	void searchHelper(Node *&node, std::vector<int> &ids, int depth, std::vector<float> &target, float distanceTol) {
+		if (node == NULL) {
+			return;
+		} else {
+			if (isInBox(node->point, target, distanceTol)) {
+				float dx = node->point[0] - target[0];
+				float dy = node->point[1] - target[1];
+				float d = sqrt(dx * dx + dy * dy);
+				if (d <= distanceTol) {
+					ids.push_back(node->id);
+				}
+				searchHelper(node->left, ids, depth+1, target, distanceTol);
+				searchHelper(node->right, ids, depth+1, target, distanceTol);
+			} else {
+				int dim = depth % 2;
+				if (node->point[dim] > target[dim]) {
+					searchHelper(node->left, ids, depth+1, target, distanceTol);
+				} else {
+					searchHelper(node->right, ids, depth+1, target, distanceTol);
+				}
+			}
+		}
+	}
+	void searchHelper2(Node *&node, std::vector<int> &ids, int depth, std::vector<float> &target, float distanceTol) {
+		if (node == NULL) {
+			return;
+		} else {
+			if (isInBox(node->point, target, distanceTol)) {
+				float dx = node->point[0] - target[0];
+				float dy = node->point[1] - target[1];
+				float d = sqrt(dx * dx + dy * dy);
+				if (d <= distanceTol) {
+					ids.push_back(node->id);
+				}
+			}
+			int dim = depth % 2;
+			if (node->point[dim] > target[dim] - distanceTol) {
+				searchHelper(node->left, ids, depth+1, target, distanceTol);
+			}
+			if (node->point[dim] < target[dim] + distanceTol) {
+				searchHelper(node->right, ids, depth+1, target, distanceTol);
+			}
+		}
+	}
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchHelper(root, ids, 0, target, distanceTol);
+		//searchHelper2(root, ids, 0, target, distanceTol);
 		return ids;
 	}
 	
